4f6f9dfa22143296f9440ac4f1494f672b0975dd
Marco Vasquez <54719844+MarcoFidelVasquezRivera@users.noreply.github.com>
2020-02-08 14:54:35 -0500

    the exception UserNotFoundException was added

--- TURN_CONTROL_SYSTEM/src/customExceptions/UserNotFoundException.java
+++ TURN_CONTROL_SYSTEM/src/customExceptions/UserNotFoundException.java
@@ -1,5 +1,24 @@
 package customExceptions;
 
-public class UserNotFoundException {
-
+@SuppressWarnings("serial")
+public class UserNotFoundException extends Exception {
+//+++++++++++++++++++++++++++++++++++
+//	 ATTRIBUTES
+//+++++++++++++++++++++++++++++++++++
+	private String userId;
+	private String userName;
+//+++++++++++++++++++++++++++++++++++
+//	  METHODS
+//+++++++++++++++++++++++++++++++++++
+	public UserNotFoundException(String userId, String userName) {
+		super("the user was not found: ");
+		this.userId=userId;
+		this.userName=userName;
+	}
+	
+	@Override
+	public String getMessage() {
+		return super.getMessage()+"\n Name:"+userName+"\n id"+userId;
+	}
+	
 }
