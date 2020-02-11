94132e1406026ba84a390517593aa7b7c6899ead
Marco Vasquez <54719844+MarcoFidelVasquezRivera@users.noreply.github.com>
2020-02-11 10:28:32 -0500

    method passTurn created and method assignTurn is almost finished

--- TURN_CONTROL_SYSTEM/src/model/ControlSystem.java
+++ TURN_CONTROL_SYSTEM/src/model/ControlSystem.java
@@ -12,12 +12,17 @@ public class ControlSystem {
 //+++++++++++++++++++++++++++++++++++
 	private ArrayList<User> users = new ArrayList<User>();
 	private ArrayList<Turn> turnsAttended = new ArrayList<Turn>();
+	private int letter;
+	private int nOne;
+	private int nTwo;
 	private char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 //+++++++++++++++++++++++++++++++++++
 //	  METHODS
 //+++++++++++++++++++++++++++++++++++
 	public ControlSystem() {
-		
+		letter=0;
+		nOne=0;
+		nTwo=0;
 	}
 	
 	
@@ -63,9 +68,24 @@ public class ControlSystem {
 			if(users.get(i).getId().equalsIgnoreCase(id) && users.get(i).getTypeId().equalsIgnoreCase(typeId) && users.get(i).getTurn()!=null) {
 				throw new UserAlreadyHasATurnException(id,typeId,users.get(i).getFistNames(),users.get(i).getTurn().getNumber());
 				
-			}else if(users.get(i).getId().equalsIgnoreCase(id) && users.get(i).getTypeId().equalsIgnoreCase(typeId) && users.get(i).getTurn()==null) {
-				
-				users.get(i).setTurn(String number,users.get(i).getFistNames(),id,Turn.NOT_ATTENDED_YET);
+			}else if(users.get(i).getId().equalsIgnoreCase(id) && users.get(i).getTypeId().equalsIgnoreCase(typeId) && users.get(i).getTurn()==null) {	
+				users.get(i).setTurn(String.valueOf(alphabet[letter])+Integer.toString(nTwo)+Integer.toString(nOne), users.get(i).getFistNames()+users.get(i).getLastNames(), users.get(i).getId(), Turn.NOT_ATTENDED_YET);
+				passTurn();	
+			}
+		}
+	}
+	
+	public void passTurn() {
+		nOne++;
+		if(nOne>9) {
+			nOne=0;
+			nTwo++;
+			if(nTwo>9) {
+				nTwo=0;
+				letter++;
+				if(letter>alphabet.length-1) {
+					letter=0;
+				}
 			}
 		}
 	}
